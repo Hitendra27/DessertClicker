66e790baf9c9856273b1616da72c0426f5d2323e
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2024-06-03 11:32:09 +0100

    feat: Implemented DessertUiState.kt

---
+++ app/src/main/java/com/example/dessertclicker/ui/DessertUiState.kt
@@ -0,0 +1,9 @@
+package com.example.dessertclicker.ui
+
+data class DessertUiState(
+    val revenue: Int,
+    val dessertsSold: Int,
+    val currentDessertIndex: Int,
+    val currentDessertPrice: Int,
+    val currentDessertImageId: Int
+)
